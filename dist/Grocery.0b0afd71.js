6c6bd99717d2023307062c63a2402102876c1bdb
soum3n0 <soumen0dey22@gmail.com>
2024-08-21 16:19:56 +0530

    biteride

---
+++ dist/Grocery.0b0afd71.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequiredfb8;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=()=>(0,a.jsx)("div",{className:"m-8 px-8",children:"Grocery Store"})});
+//# sourceMappingURL=Grocery.0b0afd71.js.map
